e422eb6071e7ebfebba6653f4871f52278a292bf
vsutinah <52406232+vsutinah@users.noreply.github.com>
2020-12-28 16:25:09 +1100

    Add skills UL to index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,20 @@ export default function Home() {
   return (
     <Fragment>
       <Layout>
+        <div>
         <h1>Hi! My name is Vincent!</h1>
-        <p>I'm a fresh graduate looking for opportunities that will allow me to gain experience as a developer.</p>
-        <p>I will be updating this website with my portfolio, so stay tuned!</p>
+        <p>I'm a fresh Computer Science graduate looking for opportunities that will allow me to gain experience as a developer.</p>
+        <p>I will be updating this website with more content, so stay tuned!</p>
+        </div>
+        <div>
+        <h1>Skills</h1>
+        <ul>
+        <li>React</li>
+        <li>NodeJS</li>
+        <li>Django</li>
+        <li>Python</li>
+        </ul>
+        </div>
       </Layout>
     </Fragment>
   )
