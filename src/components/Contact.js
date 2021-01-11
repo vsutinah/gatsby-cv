e6bf36edb3ea5f2ccb0510a386dc4d61bca18405
vsutinah <52406232+vsutinah@users.noreply.github.com>
2021-01-11 11:06:02 +1100

    Changed Contact section to use Cards

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,25 +1,31 @@
 import React from 'react'
-import { Button } from 'react-bootstrap'
-import { FaLinkedinIn, FaGithub } from 'react-icons/fa'
+import { Card, Col, Row } from 'react-bootstrap'
+import { FaLinkedinIn, FaGithub, FaEnvelope } from 'react-icons/fa'
+import ContactStyles from './contact.module.css'
 
 const Contact = () => {
     return (
         <div>
-            <h5>I&apos;d love to talk! Email me at the address below:</h5>
-            <p>
-                <a href="mailto:vincentsutinah@gmail.com">vincentsutinah@gmail.com</a>
-            </p>
-            <h5>You can also view my online profiles here:</h5>
-            <p>
-                <Button href="https://www.linkedin.com/in/vincentsutinah/" target='_blank'>
-                    <FaLinkedinIn /> LinkedIn
-                </Button>
-            </p>
-            <p>
-                <Button variant='dark' href="https://github.com/vsutinah/" target='_blank'>
-                    <FaGithub /> GitHub
-                </Button>
-            </p>
+            <Card>
+                <Card.Title className={'mt-3'}>You can contact me through these platforms!</Card.Title>
+                <Row>
+                    <Col className={'mb-3'}>
+                        <a href="mailto:vincentsutinah@gmail.com"><FaEnvelope className={ContactStyles.icon}/></a>
+                        <br/>
+                        <Card.Subtitle className={'mt-1'}>Email</Card.Subtitle>
+                    </Col>
+                    <Col className={'mb-3'}>
+                        <a href="https://www.linkedin.com/in/vincentsutinah/"><FaLinkedinIn className={ContactStyles.icon}/></a>
+                        <br/>
+                        <Card.Subtitle className={'mt-1'}>LinkedIn</Card.Subtitle>    
+                    </Col>
+                    <Col className={'mb-3'}>
+                        <a href="https://github.com/vsutinah/"><FaGithub className={ContactStyles.icon}/></a>
+                        <br/>
+                        <Card.Subtitle className={'mt-1'}>GitHub</Card.Subtitle>
+                    </Col>
+                </Row>
+            </Card>
         </div>
     )
 }
