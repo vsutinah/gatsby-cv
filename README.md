61100b6b71d12f512bf8fde2b6b15ff91c3f8194
vsutinah <52406232+vsutinah@users.noreply.github.com>
2021-01-12 12:03:44 +1100

    Added card hover animation, card shadow css and logo fixes

--- README.md
+++ README.md
@@ -9,3 +9,8 @@
 </h1>
 
 Using the basic Gatsby Hello World template to generate my own online resume/portfolio.
+
+Icons / Logos used are from:
+<a href="https://iconscout.com/icons/css" target="_blank">Css Logo Icon</a> by <a href="https://iconscout.com/contributors/pixel-icons" target="_blank">Pixel Icons</a>
+<a href="https://iconscout.com/icons/html" target="_blank">Html</a> by <a href="https://iconscout.com/contributors/pixel-icons">Pixel Icons</a> on <a href="https://iconscout.com">Iconscout</a>
+<a href="https://iconscout.com/icons/python" target="_blank">Python</a> by <a href="https://iconscout.com/contributors/icons8">Icons8</a> on <a href="https://iconscout.com">Iconscout</a>
--- src/components/Education.js
+++ src/components/Education.js
@@ -7,16 +7,16 @@ const Education = () => {
             <CardDeck>
                 <Card>
                     <Card.Body>
-                        <Card.Title>Diploma of Information Technology</Card.Title>
-                        <Card.Subtitle className='mb-2 text-muted'>Oct 2016 - June 2017</Card.Subtitle>
-                        <Card.Text>Monash College<br/>Melbourne, Victoria</Card.Text>
+                        <Card.Title>Bachelor of Computer Science</Card.Title>
+                        <Card.Subtitle className='mb-2 text-muted'>Jul 2017 - Dec 2019</Card.Subtitle>
+                        <Card.Text>Monash University<br/>Melbourne, Victoria</Card.Text>
                     </Card.Body>
                 </Card>
                 <Card>
                     <Card.Body>
-                        <Card.Title>Bachelor of Computer Science</Card.Title>
-                        <Card.Subtitle className='mb-2 text-muted'>Jul 2017 - Dec 2019</Card.Subtitle>
-                        <Card.Text>Monash University<br/>Melbourne, Victoria</Card.Text>
+                        <Card.Title>Diploma of Information Technology</Card.Title>
+                        <Card.Subtitle className='mb-2 text-muted'>Oct 2016 - June 2017</Card.Subtitle>
+                        <Card.Text>Monash College<br/>Melbourne, Victoria</Card.Text>
                     </Card.Body>
                 </Card>
             </CardDeck>
